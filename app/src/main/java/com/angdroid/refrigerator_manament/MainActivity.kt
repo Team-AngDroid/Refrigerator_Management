9281ec268358c6bc8367579c1c41ca4bca1a7202
minho <my_alsgh@naver.com>
2022-09-27 04:17:34 +0900

    first commit

---
+++ app/src/main/java/com/angdroid/refrigerator_manament/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.angdroid.refrigerator_manament
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
