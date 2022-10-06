874479f2c6e8bbfdc635ef1329139b97bd68b586
minho <my_alsgh@naver.com>
2022-10-07 06:54:16 +0900

    [FEAT] Repository Impl Create

--- app/src/main/java/com/angdroid/refrigerator_manament/data/repository/camera/CameraRepositoryImpl.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/data/repository/camera/CameraRepositoryImpl.kt
@@ -1,6 +1,10 @@
 package com.angdroid.refrigerator_manament.data.repository.camera
 
+import com.angdroid.refrigerator_manament.data.datasource.camera.CameraDataSource
 import com.angdroid.refrigerator_manament.domain.repository.CameraRepository
 
-class CameraRepositoryImpl:CameraRepository {
+class CameraRepositoryImpl(private val cameraDataSource: CameraDataSource) : CameraRepository {
+    override suspend fun getDetectImage(): List<ByteArray> {
+        TODO("Not yet implemented")
+    }
 }
