32bfb6dd94ab00c342f8298b62de910f20195548
minho <my_alsgh@naver.com>
2022-10-07 04:13:10 +0900

    [CHORE] List Data Class, Temporary list item Create

---
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/model/Food.kt
@@ -0,0 +1,6 @@
+package com.angdroid.refrigerator_manament.presentation.home.model
+
+import com.angdroid.refrigerator_manament.data.dto.CategoryType
+import java.time.LocalDate
+
+data class Food(val id: String, val expirationDate: LocalDate, val name: String, val image: String, val categoryType: CategoryType)
