0df6fdefd7034eb70c24a626432ff995406e15f1
minho <my_alsgh@naver.com>
2022-10-08 17:13:47 +0900

    [FEAT] UseCase Constructor Inject add

--- app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/GetUserFoodCategoryListImpl.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/GetUserFoodCategoryListImpl.kt
@@ -1,6 +1,10 @@
 package com.angdroid.refrigerator_manament.domain.usecase
 
-class GetUserFoodCategoryListImpl : GetUserFoodCategoryList {
+import com.angdroid.refrigerator_manament.domain.repository.FireBaseRepository
+import javax.inject.Inject
+
+class GetUserFoodCategoryListImpl @Inject constructor(private val fireBaseRepository: FireBaseRepository) :
+    GetUserFoodCategoryList {
     override fun invoke(): List<String> {
         TODO("Not yet implemented")
     }
