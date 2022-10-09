b8fb3ca4bae3bc5a5efdf217c35954dbef2380bb
minho <my_alsgh@naver.com>
2022-10-09 22:26:03 +0900

    [FEAT] Multi View Holder Test File

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/MainActivity.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/MainActivity.kt
@@ -3,10 +3,17 @@ package com.angdroid.refrigerator_manament.presentation.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.angdroid.refrigerator_manament.R
+import com.angdroid.refrigerator_manament.presentation.home.model.BaseType
+import com.angdroid.refrigerator_manament.presentation.home.model.Category
+import com.angdroid.refrigerator_manament.presentation.home.model.Food
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
+    private val array = ArrayList<BaseType>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        array.add(Category("채소", 2, 1))
+        array.add(Food(1, LocalDate.now(), "", "", 1, 4))
     }
 }
--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/model/Food.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/model/Food.kt
@@ -1,6 +1,15 @@
 package com.angdroid.refrigerator_manament.presentation.home.model
 
-import com.angdroid.refrigerator_manament.data.dto.CategoryType
 import java.time.LocalDate
 
-data class Food(val id: String, val expirationDate: LocalDate, val name: String, val image: String)
+data class Food(
+    val id: Int,
+    val expirationDate: LocalDate,
+    val name: String,
+    val image: String,
+    val categoryId: Int,
+    val foodCount: Int
+) : BaseType {
+    override fun getType(): Int = 0
+    override fun getCount(): Int = foodCount
+}
