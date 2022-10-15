8bce1f27fa3e4378e1a2dd3931e04aaa07c46886
IslandofDream <rhffldkt0517@naver.com>
2022-10-15 10:09:12 +0900

    [Feat] Revert MainActivity, Add Home Activity - 논의결과에 따라 MainActivity 초기 상태로 revert - Home Activity 추가

---
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.angdroid.refrigerator_manament.presentation.home
+
+import android.os.Bundle
+import com.angdroid.refrigerator_manament.R
+import com.angdroid.refrigerator_manament.databinding.ActivityHomeBinding
+import com.angdroid.refrigerator_manament.presentation.util.BaseActivity
+
+class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
