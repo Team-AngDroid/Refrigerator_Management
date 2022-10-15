d74b3ac5bc1b2c5770568292b881acdd7260d3da
minho <my_alsgh@naver.com>
2022-10-15 16:08:09 +0900

    [Feat] Calculate that Food Field foodCount accumulator each foodId

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/MainActivity.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/MainActivity.kt
@@ -2,6 +2,7 @@ package com.angdroid.refrigerator_manament.presentation.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.angdroid.refrigerator_manament.R
 import com.angdroid.refrigerator_manament.databinding.ActivityMainBinding
 import com.angdroid.refrigerator_manament.presentation.DynamicGridLayoutManager
@@ -15,7 +16,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     private val array = arrayListOf<BaseType>(
         Food("123", 107, LocalDate.now(), "계란", "", 3, 6),
+        Food("123", 107, LocalDate.now(), "계란", "", 3, 1),
+        Food("123", 107, LocalDate.now(), "계란", "", 3, 2),
+        Food("123", 107, LocalDate.now(), "계란", "", 3, 6),
         Food("123", 103, LocalDate.now(), "무우", "", 1, 4),
+        Food("123", 103, LocalDate.now(), "무우", "", 1, 1),
+        Food("123", 103, LocalDate.now(), "무우", "", 1, 2),
+        Food("123", 103, LocalDate.now(), "무우", "", 1, 3),
         Food("123", 104, LocalDate.now(), "사과", "", 2, 4),
         Food("123", 105, LocalDate.now(), "배", "", 2, 1),
         Food("123", 110, LocalDate.now(), "치즈", "", 4, 1),
@@ -31,22 +38,57 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         intenttt.putExtra("data", (array[1] as Food))
         startActivity(intenttt)
         finish()*/
+
+        val result = array.groupingBy { it.id }
+            .aggregate { _, accumulator: BaseType?, element, first ->
+                if (first) element
+                else {
+                    (accumulator as Food).foodCount = accumulator.count + element.count
+                    Log.e("Accumulator", accumulator.toString())
+                    accumulator
+                }
+            }.values.toMutableList()
+        result.sortBy { it.category }
+
+
+        Log.e("Result!!", "$result")
         var categoryTemp: Pair<Int, Int> =
-            Pair(array[array.size - 1].category, array[array.size - 1].count)
-        for (i in array.size - 1 downTo 0) {
-            array[i].run {
+            Pair(result[result.size - 1].category, result[result.size - 1].count)
+        for (i in result.size - 1 downTo 0) {
+            result[i].run {
                 if (this.category != categoryTemp.first) {
-                    array.add(i + 1, Category("유제품", categoryTemp.second, categoryTemp.first))
+                    result.add(
+                        i + 1,
+                        Category(
+                            resources.getString(categoryList[categoryTemp.first - 1]),
+                            categoryTemp.second,
+                            categoryTemp.first
+                        )
+                    )
                     categoryTemp = Pair(this.category, this.count)
                 } else {
                     categoryTemp.second.plus(this.count)
                 }
             }
         }
-        array.add(0, Category("채소", categoryTemp.second, categoryTemp.first))
+        result.add(
+            0,
+            Category(
+                resources.getString(categoryList[categoryTemp.first - 1]),
+                categoryTemp.second,
+                categoryTemp.first
+            )
+        )
         adapter = CategoryListAdapter({}, {})
         binding.rvList.layoutManager = DynamicGridLayoutManager(this, adapter.getSpanSizeLookUp())
         binding.rvList.adapter = adapter
-        adapter.submitList(array)
+        adapter.submitList(result)
+    }
+
+    companion object CategoryType {
+        val categoryList = listOf<Int>(
+            R.string.category_vegetable, R.string.category_fruit,
+            R.string.category_meat, R.string.category_seasoning
+        )
     }
 }
