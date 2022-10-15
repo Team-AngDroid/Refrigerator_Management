bd920efa7a71daed25361a50ad9cac948c0fab15
minho <my_alsgh@naver.com>
2022-10-16 04:14:59 +0900

    [Feat] change interface implement to sealed class implement

---
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/model/IngredientType.kt
@@ -0,0 +1,26 @@
+package com.angdroid.refrigerator_manament.presentation.home.model
+
+import java.time.LocalDate
+
+sealed class IngredientType(
+    val id: Int,
+    val type: Int,  //Enum Class 으로 해도 되지만, Category 의 변화를 위해서도 쓰임
+    val count: Int, //Category : 한 카테고리의 전체 Count, Food : 한 식재료의 각각의 Count
+    open val categoryId: Int //이것만 동치관계
+) {
+    data class Category(
+        val categoryName: String,
+        val categoryCount: Int,
+        override val categoryId: Int
+    ) : IngredientType(categoryId, categoryId, categoryCount, categoryId)
+
+    data class Food(
+        val fid: String,
+        val foodId: Int,
+        val expirationDate: LocalDate,
+        val name: String,
+        val image: String,
+        override val categoryId: Int,
+        var foodCount: Int
+    ) : IngredientType(foodId, 0, foodCount, categoryId)
+}
