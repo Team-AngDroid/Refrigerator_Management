2333b6da68b862c7070c9b573202dcb97a4384a3
minho <my_alsgh@naver.com>
2022-10-16 04:14:32 +0900

    [Chore] interface type remove

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/model/BaseIngredientType.kt
+++
@@ -1,8 +0,0 @@
-package com.angdroid.refrigerator_manament.presentation.home.model
-
-interface BaseIngredientType {
-    val id: Int
-    val type: Int
-    val count: Int
-    val category: Int
-}
