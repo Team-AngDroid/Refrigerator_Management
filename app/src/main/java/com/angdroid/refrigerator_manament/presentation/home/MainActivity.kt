09d0d5830696f2d4a8576c99646000e630b9969a
minho <my_alsgh@naver.com>
2022-10-15 20:37:53 +0900

    [Add] Category String Resource Add

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/MainActivity.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/MainActivity.kt
@@ -25,9 +25,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         Food("123", 103, LocalDate.now(), "무우", "", 1, 3),
         Food("123", 104, LocalDate.now(), "사과", "", 2, 4),
         Food("123", 105, LocalDate.now(), "배", "", 2, 1),
+        Food("123", 106, LocalDate.now(), "귤", "", 2, 1),
         Food("123", 110, LocalDate.now(), "치즈", "", 4, 1),
+        Food("123", 111, LocalDate.now(), "새우", "", 5, 1),
+        Food("123", 112, LocalDate.now(), "오징어", "", 5, 2),
+        Food("123", 113, LocalDate.now(), "고등어", "", 5, 3),
+        Food("123", 109, LocalDate.now(), "우유", "", 4, 1),
         Food("123", 108, LocalDate.now(), "생닭", "", 3, 1),
         Food("123", 101, LocalDate.now(), "당근", "", 1, 4),
+        Food("123", 102, LocalDate.now(), "오이", "", 1, 4),
     )
     lateinit var adapter: CategoryListAdapter
 
--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/util/types/CategoryType.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/util/types/CategoryType.kt
@@ -6,6 +6,6 @@ import com.angdroid.refrigerator_manament.R
 object CategoryType {
     val categoryList = listOf<Int>(
         R.string.category_vegetable, R.string.category_fruit,
-        R.string.category_meat, R.string.category_seasoning
+        R.string.category_meat, R.string.category_milk, R.string.category_seafood
     )
 }
