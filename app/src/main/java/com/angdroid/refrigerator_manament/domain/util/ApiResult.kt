b4a2fbedc4ee048f3fea78f69c062667d83662a3
minho <my_alsgh@naver.com>
2022-10-17 03:36:18 +0900

    [Add] UiState Add, ApiResult Field Modify

--- app/src/main/java/com/angdroid/refrigerator_manament/domain/util/ApiResult.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/domain/util/ApiResult.kt
@@ -1,9 +1,6 @@
 package com.angdroid.refrigerator_manament.domain.util
 
 sealed class ApiResult<out T> {
-    object Init : ApiResult<Nothing>()
+    data class Fail(val message: String) : ApiResult<Nothing>()
     data class Success<T>(val data: T) : ApiResult<T>()
-    data class Loading(val isLoading: Boolean) : ApiResult<Nothing>()
-    data class Empty(val isEmpty: Boolean) : ApiResult<Nothing>()
-    data class Error(val error: String) : ApiResult<Nothing>()
 }
