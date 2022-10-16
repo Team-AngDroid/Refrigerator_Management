1378d3cc714629c96916c69bbcbd8f86794af925
IslandofDream <rhffldkt0517@naver.com>
2022-10-16 15:12:32 +0900

    [UI] Delete CameraFragment , Add Camera Activity - 카메라 프래그먼트 삭제후 카메라 액티비티 뼈대 생성

---
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/camera/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.angdroid.refrigerator_manament.presentation.camera
+
+import android.os.Bundle
+import com.angdroid.refrigerator_manament.R
+import com.angdroid.refrigerator_manament.databinding.ActivityCameraBinding
+import com.angdroid.refrigerator_manament.presentation.util.BaseActivity
+
+class CameraActivity : BaseActivity<ActivityCameraBinding>(R.layout.activity_camera) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
