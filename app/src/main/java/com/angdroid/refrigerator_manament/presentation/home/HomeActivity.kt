53f3112a7cadb22deb159d1fbececedbabecc08e
IslandofDream <rhffldkt0517@naver.com>
2022-10-16 14:07:54 +0900

    [Feat] Add Navigation Graph and set to Bottom Navigation - 네비게이션 그래프 작성 - 바텀내비게이션에 네비게이션 그래프 적용

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/HomeActivity.kt
@@ -1,12 +1,27 @@
 package com.angdroid.refrigerator_manament.presentation.home
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.angdroid.refrigerator_manament.R
 import com.angdroid.refrigerator_manament.databinding.ActivityHomeBinding
 import com.angdroid.refrigerator_manament.presentation.util.BaseActivity
 
+
 class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setNavigation()
     }
+
+    private fun setNavigation(){
+        val host = supportFragmentManager.findFragmentById(R.id.nav_container) as NavHostFragment? ?:return
+        navController = host.navController
+        binding.bottomNavHome.setupWithNavController(navController)
+    }
+
 }
