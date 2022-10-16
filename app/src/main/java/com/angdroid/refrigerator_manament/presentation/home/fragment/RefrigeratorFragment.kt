5a8da326505678ceef5e309de52d77a56266ac41
minho <my_alsgh@naver.com>
2022-10-17 03:43:02 +0900

    [Feat] Refrigerator Create

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/fragment/RefrigeratorFragment.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/fragment/RefrigeratorFragment.kt
@@ -12,9 +12,19 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class RefrigeratorFragment :
     BaseFragment<FragmentRefrigeratorBinding>(R.layout.fragment_refrigerator) {
-
+    private lateinit var adapter: CategoryListAdapter
+    private val ingredientViewModel: IngredientViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+        adapter = CategoryListAdapter({}, {})
+        binding.rvList.layoutManager =
+            DynamicGridLayoutManager(requireContext(), adapter.getSpanSizeLookUp())
+        binding.rvList.adapter = adapter
+        collectingIngredientList()
     }
 
     companion object {
