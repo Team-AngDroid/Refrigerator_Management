023b85c68b0442fba0319bf007c39d65d8b071a3
minho <my_alsgh@naver.com>
2022-10-18 06:09:39 +0900

    [Feat] Document Reference object Modify

--- app/src/main/java/com/angdroid/refrigerator_manament/application/App.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/application/App.kt
@@ -1,24 +1,41 @@
 package com.angdroid.refrigerator_manament.application
 
 import android.app.Application
+import android.content.Context
 import com.angdroid.refrigerator_manament.BuildConfig
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class App:Application() {
-    lateinit var firebaseUserReference:CollectionReference
-    lateinit var firebaseFoodReference:CollectionReference
-    lateinit var firebaseRecipeReference:CollectionReference
+class App : Application() {
+
+    init {
+        instance = this
+    }
+
     override fun onCreate() {
         super.onCreate()
-        firebaseUserReference = Firebase.firestore.collection("User")
-        firebaseFoodReference = Firebase.firestore.collection("Food")
-        firebaseRecipeReference = Firebase.firestore.collection("Recipe")
-
+        firestoreCollectionReference = Firebase.firestore.collection("refrigeratorManagement")
+        firestoreCollectionReference.run {
+            firebaseUserReference = this.document("User")
+            firebaseFoodReference = this.document("Food")
+            firebaseRecipeReference = this.document("Recipe")
+        }
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    companion object {
+        lateinit var firestoreCollectionReference: CollectionReference
+        lateinit var firebaseUserReference: DocumentReference
+        lateinit var firebaseFoodReference: DocumentReference
+        lateinit var firebaseRecipeReference: DocumentReference
+        private lateinit var instance: App
+        fun getInstance(): Context {
+            return instance.applicationContext
+        }
     }
 }
