35fe5e5552d2094c88e158e467220acc9d6aa0f5
IslandofDream <rhffldkt0517@naver.com>
2022-10-17 15:34:01 +0900

    [UI] AppBar Divide / Applay Appbar - 앱바 종류별 분리 - 앱바 아이콘 적용

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/camera/AddIngredientActivity.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/camera/AddIngredientActivity.kt
@@ -1,12 +1,14 @@
 package com.angdroid.refrigerator_manament.presentation.camera
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import com.angdroid.refrigerator_manament.R
 import com.angdroid.refrigerator_manament.databinding.ActivityAddIngredientBinding
 import com.angdroid.refrigerator_manament.presentation.util.BaseActivity
 
 class AddIngredientActivity : BaseActivity<ActivityAddIngredientBinding>(R.layout.activity_add_ingredient) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/HomeActivity.kt
@@ -5,18 +5,16 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import com.angdroid.refrigerator_manament.R
 import com.angdroid.refrigerator_manament.databinding.ActivityHomeBinding
-import com.angdroid.refrigerator_manament.presentation.camera.CameraActivity
+import com.angdroid.refrigerator_manament.presentation.camera.AddIngredientActivity
 import com.angdroid.refrigerator_manament.presentation.util.BaseActivity
 
 
 class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
 
     private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +29,7 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
         binding.bottomNavHome.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            binding.appbarHome.appbar.title = when (destination.id) {
+            binding.appbarHome.title = when (destination.id) {
                 R.id.fragment_recipe -> destination.label
                 R.id.fragment_refrigerator -> destination.label
                 else -> throw IllegalAccessException("Error.NavController")
@@ -39,9 +37,9 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(R.layout.activity_home) {
         }
     }
 
-    private fun setOnClickListener(){
+    private fun setOnClickListener() {
         binding.fabCamera.setOnClickListener {
-            startActivity(Intent(this, CameraActivity::class.java))
+            startActivity(Intent(this, AddIngredientActivity::class.java))
         }
     }
 
