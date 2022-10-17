2c1fe8fdf63fd746942203c51818f0821cd3e910
minho <my_alsgh@naver.com>
2022-10-18 05:54:10 +0900

    [Feat] Each Document Reference Define on Application Class

--- app/src/main/java/com/angdroid/refrigerator_manament/application/App.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/application/App.kt
@@ -2,13 +2,23 @@ package com.angdroid.refrigerator_manament.application
 
 import android.app.Application
 import com.angdroid.refrigerator_manament.BuildConfig
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
 class App:Application() {
+    lateinit var firebaseUserReference:CollectionReference
+    lateinit var firebaseFoodReference:CollectionReference
+    lateinit var firebaseRecipeReference:CollectionReference
     override fun onCreate() {
         super.onCreate()
+        firebaseUserReference = Firebase.firestore.collection("User")
+        firebaseFoodReference = Firebase.firestore.collection("Food")
+        firebaseRecipeReference = Firebase.firestore.collection("Recipe")
+
         if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 }
