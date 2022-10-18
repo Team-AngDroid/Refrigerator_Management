bc050221b38a9dac2375d5bd1d821f1d4d5b4c4b
minho <my_alsgh@naver.com>
2022-10-19 03:47:46 +0900

    [Feat] DataSource Get FireStore User Document

--- app/src/main/java/com/angdroid/refrigerator_manament/data/datasource/home/UserInfoDataSourceImpl.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/data/datasource/home/UserInfoDataSourceImpl.kt
@@ -1,12 +1,11 @@
 package com.angdroid.refrigerator_manament.data.datasource.home
 
+import com.angdroid.refrigerator_manament.application.App
 import com.angdroid.refrigerator_manament.data.dto.FoodDto
 import com.angdroid.refrigerator_manament.data.dto.FoodInfoDto
 import com.angdroid.refrigerator_manament.data.dto.UserDto
 import javax.inject.Inject
 
 class UserInfoDataSourceImpl @Inject constructor() : UserInfoDataSource {
-    override suspend fun getUserInfo(): UserDto {
-        return UserDto("", "", "", listOf(FoodInfoDto(0, FoodDto("", 1, "", "", "", 0, 1))))
-    }
+    override suspend fun getUserInfo()= App.fireStoreUserReference.get()
 }
