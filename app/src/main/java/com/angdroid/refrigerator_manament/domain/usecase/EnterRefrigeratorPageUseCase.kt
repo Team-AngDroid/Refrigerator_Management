90668c83a850309ab476c44cd2e841e37aaaed8e
minho <my_alsgh@naver.com>
2022-10-19 03:51:35 +0900

    [Feat] Business Logic Add And Flow Emit Change

--- app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/EnterRefrigeratorPageUseCase.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/EnterRefrigeratorPageUseCase.kt
@@ -1,8 +1,10 @@
 package com.angdroid.refrigerator_manament.domain.usecase
 
+import androidx.lifecycle.LifecycleCoroutineScope
 import com.angdroid.refrigerator_manament.domain.entity.model.IngredientType
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 
 interface EnterRefrigeratorPageUseCase {
-    suspend operator fun invoke(): Flow<List<IngredientType>>
+    suspend operator fun invoke(coroutineScope: CoroutineScope, onComplete: suspend (Flow<MutableList<IngredientType>>) -> Unit)
 }
