f3fb536b67b2b6172aadf99fef8f1a5e24699260
minho <my_alsgh@naver.com>
2022-10-21 00:25:21 +0900

    [Feat] ViewModel Logic Add

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/camera/viewmodel/CameraViewModel.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/camera/viewmodel/CameraViewModel.kt
@@ -1,4 +1,41 @@
 package com.angdroid.refrigerator_manament.presentation.camera.viewmodel
 
-class CameraViewModel {
+import androidx.lifecycle.ViewModel
+import com.angdroid.refrigerator_manament.domain.entity.model.IngredientType
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class CameraViewModel : ViewModel() {
+
+    private val _foodList = MutableStateFlow<List<IngredientType.Food>>(emptyList())
+    val foodList get() = _foodList
+
+
+    fun setIntentFoodList(intentList: List<IngredientType.Food>) {
+        _foodList.value = intentList
+    }
+
+    fun removeItem(currentItem: IngredientType.Food) {
+        val currentList = _foodList.value.toMutableList()
+        currentList.remove(currentItem)
+        _foodList.value = currentList
+    }
+
+    fun minusItemCount(currentItem: IngredientType.Food, position: Int) {
+        val currentList = _foodList.value.toMutableList()
+        if (currentItem.foodCount == 1)
+            removeItem(currentItem)
+        else {
+            currentList[position] =
+                IngredientType.Food(
+                    currentItem.fid,
+                    currentItem.foodId,
+                    currentItem.expirationDate,
+                    currentItem.name,
+                    currentItem.image,
+                    currentItem.categoryId,
+                    currentItem.foodCount - 1
+                )
+            _foodList.value = currentList
+        }
+    }
 }
