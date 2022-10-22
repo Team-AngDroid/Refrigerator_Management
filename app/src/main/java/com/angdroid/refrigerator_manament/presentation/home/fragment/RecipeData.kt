0af11fe98caf377cca4b167898924dbf52802f77
IslandofDream <rhffldkt0517@naver.com>
2022-10-22 21:57:42 +0900

    [Feat] Add Temporary/Dummy Data for logic - 로직구현을 위한 임시 및 더미 데이터 추가

---
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/fragment/RecipeData.kt
@@ -0,0 +1,5 @@
+package com.angdroid.refrigerator_manament.presentation.home.fragment
+
+data class RecipeData(val name:String, val img:String, val time:String)
+
+//구현용 임시 데이터!!! 나중에 삭제 예정
--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/fragment/RecipeFragment.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/fragment/RecipeFragment.kt
@@ -10,6 +10,15 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RecipeFragment : BaseFragment<FragmentRecipeBinding>(R.layout.fragment_recipe) {
+
+    private val recipeList = listOf<RecipeData>(
+        RecipeData("당큰케이크", "당근", "5분"),
+        RecipeData("과일 사라다", "사과", "5분"),
+        RecipeData("체다슬라이스칩스", "치즈", "5분"),
+        RecipeData("무생채", "무우", "5분"),
+        RecipeData("에어프라이어치킨", "생닭", "5분"),
+    )
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
