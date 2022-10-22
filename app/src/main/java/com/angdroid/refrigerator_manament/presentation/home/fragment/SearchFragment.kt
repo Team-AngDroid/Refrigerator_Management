bbe5138770d983aa39cf164acd2005967f4152bd
IslandofDream <rhffldkt0517@naver.com>
2022-10-22 17:52:22 +0900

    [Feat] Add Fragmetn and set navigation - SearchFragment 추가 - 네비게이션 연결

---
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/fragment/SearchFragment.kt
@@ -0,0 +1,18 @@
+package com.angdroid.refrigerator_manament.presentation.home.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.angdroid.refrigerator_manament.R
+import com.angdroid.refrigerator_manament.databinding.FragmentSearchBinding
+import com.angdroid.refrigerator_manament.presentation.util.BaseFragment
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
