33c03e8144784b1ae5a4f78bf74e28058f57bdf2
minho <my_alsgh@naver.com>
2022-10-23 23:19:48 +0900

    [Feat] Complete Detail View Logic

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/detail/DetailActivity.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/detail/DetailActivity.kt
@@ -2,9 +2,13 @@ package com.angdroid.refrigerator_manament.presentation.detail
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.window.layout.WindowMetrics
+import androidx.window.layout.WindowMetricsCalculator
 import com.angdroid.refrigerator_manament.R
 import com.angdroid.refrigerator_manament.databinding.ActivityDetailBinding
 import com.angdroid.refrigerator_manament.domain.entity.model.IngredientType
+import com.angdroid.refrigerator_manament.presentation.detail.adapter.DetailIngredientListAdapter
+import com.angdroid.refrigerator_manament.presentation.detail.adapter.DetailListAdapter
 import com.angdroid.refrigerator_manament.presentation.util.BaseActivity
 import com.angdroid.refrigerator_manament.util.collectFlowWhenStarted
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,12 +18,18 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_det
 
     private val detailViewModel: DetailViewModel by viewModels()
     private lateinit var detailListAdapter: DetailListAdapter
+    private lateinit var ingredientDetailListAdapter: DetailIngredientListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.lifecycleOwner = this
         binding.detailViewModel = detailViewModel
         detailListAdapter = DetailListAdapter()
+
+        val windowMetrics: WindowMetrics = WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(this)
+        ingredientDetailListAdapter = DetailIngredientListAdapter(windowMetrics)
+
         binding.rcRecipe.adapter = detailListAdapter
+        binding.rvIngredient.adapter = ingredientDetailListAdapter
         collectData()
         intent.getParcelableExtra<IngredientType.Food>("foodName").let { food ->
             if (food == null) {
@@ -43,7 +53,10 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_det
 
     private fun collectData() {
         collectFlowWhenStarted(detailViewModel.recipeList) {
-            detailListAdapter.submitList(it)
+             detailListAdapter.submitList(it)
+        }
+        collectFlowWhenStarted(detailViewModel.foodList) {
+            ingredientDetailListAdapter.submitList(it)
         }
     }
 }
