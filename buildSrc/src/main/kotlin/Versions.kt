36ff05fb6ae942665e37ab2ae4cf5f42baf130c4
minho <my_alsgh@naver.com>
2022-10-31 21:21:29 +0900

    [Feat] Dependencies Add Plus Other Implementation

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,9 +5,9 @@ object Versions {
     const val kotlinVersion = "1.6.20"
     const val kotlinCoroutinesVersion = "1.5.0"
     const val coreKtxVersion = "1.7.0"
-    const val appCompatVersion = "1.4.2"
+    const val appCompatVersion = "1.5.1"
     const val materialDesignVersion = "1.4.0"
-    const val constraintLayoutVersion = "2.1.3"
+    const val constraintLayoutVersion = "2.1.4"
     const val legacySupportVersion = "1.0.0"
     const val hiltVersion = "2.42"
     const val navVersion = "2.4.1"
@@ -17,7 +17,7 @@ object Versions {
     const val coroutinesAndroidVersion = "1.3.9"
     const val recyclerView = "1.2.1"
     const val junit = "1.1.3"
-
+    const val coroutinePlayService = "1.4.1"
     const val lottieVersion = "5.2.0"
     const val retrofitVersion = "2.9.0"
     const val okHttpVersion = "4.9.3"
@@ -31,9 +31,13 @@ object Versions {
     const val gsonVersion = "2.8.9"
     const val gsonConverterVersion = "2.9.0"
     const val roomVersion = "2.4.2"
+    const val window = "1.0.0"
 
     val javaVersion = JavaVersion.VERSION_11
 
     const val mockitoCoreVersion = "2.28.2"
     const val mockitoAndroidVersion = "2.24.5"
+    const val fireStore = "24.4.0"
+    const val fireBaseMessaging = "23.1.0"
+    const val fireBaseBom = "31.0.1"
 }
