348bf381b80bef1a169f813b145e902407167f36
IslandofDream <rhffldkt0517@naver.com>
2022-11-01 12:24:46 +0900

    [Feat] Apply AddIngredient List BindingAdapter

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/camera/viewmodel/AddIngredientViewModel.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/camera/viewmodel/AddIngredientViewModel.kt
@@ -31,9 +31,11 @@ class AddIngredientViewModel : ViewModel() {
         val currentList = _foodList.value.toMutableList()
         val position = _foodList.value.indexOf(currentItem)
         //adapter는 position을 기억하고 있다. 그래서 이전 position 가져오게 된당
-        if (currentItem.foodCount == 1)
-            removeItem(currentItem)
-        else {
+//        if (currentItem.foodCount == 1)
+//            removeItem(currentItem)
+        // 디자이너분 말처럼 1일때 마이너스 버튼을 비활성화 시켜야 한다면 1일때 마이너스 시키면 삭제가 아닌
+        // 아무런 동작을 해주는게 맞지 않나 싶어서 우선 바꿈
+        if(currentItem.foodCount !=1) {
             currentList[position] =
                 IngredientType.Food(
                     currentItem.fid,
