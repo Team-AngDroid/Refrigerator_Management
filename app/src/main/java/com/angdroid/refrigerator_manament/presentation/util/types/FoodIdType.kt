9cbfeb5f660417498d098de947889f754f9280a3
minho <my_alsgh@naver.com>
2022-11-05 02:25:53 +0900

    [Chore] Add Extra Food Type

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/util/types/FoodIdType.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/util/types/FoodIdType.kt
@@ -1,8 +1,19 @@
 package com.angdroid.refrigerator_manament.presentation.util.types
 
-enum class FoodIdType (val foodId: Int) {
+enum class FoodIdType(val foodId: Int) {
     당근(101), 오이(102), 무우(103),
-    사과(104), 배(105), 귤(106),
-    계란(107), 생닭(108), 우유(109),
-    치즈(110), 새우(111), 오징어(112), 고등어(113),
+    사과(104), 배(105), 귤(106), 바나나(107),
+    오렌지(108), 포도(109), 망고(110), 키위(111),
+    계란(112), 생닭(113), 우유(114),
+    치즈(115), 새우(116), 오징어(117), 고등어(118);
+
+    fun returnCategoryType() = when (foodId) {
+        in 101..103 -> 1
+        in 104..111 -> 2
+        in 112..113 -> 3
+        in 114..115 -> 4
+        in 116..118 -> 5
+        else -> 0
+    }
+    fun findFoodId(name:String) = valueOf(name).foodId
 }
