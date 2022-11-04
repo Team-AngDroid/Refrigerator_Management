85ac4665ca9e16983499d1b4c2b6ffb621167dc0
minho <my_alsgh@naver.com>
2022-11-05 03:21:06 +0900

    [Feat] UseCase Setting

--- app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/GetRecommendedRecipeListUseCaseImpl.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/GetRecommendedRecipeListUseCaseImpl.kt
@@ -1,11 +1,15 @@
 package com.angdroid.refrigerator_manament.domain.usecase
 
+import com.angdroid.refrigerator_manament.domain.entity.RecipeEntity
+import com.angdroid.refrigerator_manament.domain.entity.model.IngredientType
 import com.angdroid.refrigerator_manament.domain.repository.FireBaseRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class GetRecommendedRecipeListUseCaseImpl @Inject constructor(private val fireBaseRepository: FireBaseRepository):
+class GetRecommendedRecipeListUseCaseImpl @Inject constructor(private val fireBaseRepository: FireBaseRepository) :
     GetRecommendedRecipeListUseCase {
-    override fun invoke(): List<String> {
-        TODO("Not yet implemented")
+    override fun invoke(ingredient: String): Flow<List<RecipeEntity>> = flow {
+        emit(fireBaseRepository.getIngredientRecipe(ingredient))
     }
 }
--- app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/GetUserFoodDetailListUseCase.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/GetUserFoodDetailListUseCase.kt
@@ -1,9 +1,12 @@
 package com.angdroid.refrigerator_manament.domain.usecase
 
+import com.angdroid.refrigerator_manament.domain.entity.model.IngredientType
+import kotlinx.coroutines.flow.Flow
+
 /**
  * 음식상세페이저에서 표시할 각각 음식에 대한 [음식의 갯수,이미지,유통기한]을 가져올 함수 입니다.
  * 마이노 식재료 Detail
  */
 interface GetUserFoodDetailListUseCase {
-    operator fun invoke(): Unit // List<Food>
+    operator fun invoke(ingredient:String): Flow<List<IngredientType.Food>>
 }
