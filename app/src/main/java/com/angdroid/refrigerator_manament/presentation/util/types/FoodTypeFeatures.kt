3c5bbaa7fdcc896e5e4f2c41cc194c71c5a3d076
minho <my_alsgh@naver.com>
2022-11-05 05:04:18 +0900

    [Feat] Extra Food Add

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/util/types/FoodTypeFeatures.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/util/types/FoodTypeFeatures.kt
@@ -6,7 +6,7 @@ import com.angdroid.refrigerator_manament.R
 
 enum class FoodTypeFeatures(@DrawableRes val imageRes: Int) {
     당근(R.drawable.carrot), 오이(R.drawable.cucumber), 무우(R.drawable.moo),
-    사과(R.drawable.apple), 배(R.drawable.pear), 귤(R.drawable.tangerine), 바나나(107),
+    사과(R.drawable.apple), 배(R.drawable.pear), 귤(R.drawable.tangerine), 바나나(R.drawable.banana),
     오렌지(R.drawable.orange), 포도(R.drawable.grape), 망고(R.drawable.mango), 키위(R.drawable.kiwi),
     계란(R.drawable.egg), 생닭(R.drawable.chicken), 우유(R.drawable.milk),
     치즈(R.drawable.cheese), 새우(R.drawable.shrimp), 오징어(R.drawable.squid), 고등어(R.drawable.mackerel),
