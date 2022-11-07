4f582ed0ac0224349aab06747c5fe77d9d8a9458
minho <my_alsgh@naver.com>
2022-11-08 04:44:28 +0900

    [Feat] Save Image File in Cache Dir

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/camera/CameraActivity.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/camera/CameraActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import com.angdroid.refrigerator_manament.R
 import com.angdroid.refrigerator_manament.databinding.ActivityCameraBinding
@@ -37,10 +38,18 @@ class CameraActivity : BaseActivity<ActivityCameraBinding>(R.layout.activity_cam
             IngredientType.Food("12", 101, LocalDate.now(), "당근", "", 1, 4),
             IngredientType.Food("13", 102, LocalDate.now(), "오이", "", 1, 4)
         )
-        saveBitmapToJpeg(BitmapFactory.decodeResource(resources, FoodTypeFeatures.계란.imageRes))
+        val filePath = saveBitmapToJpeg(
+            FoodTypeFeatures.values()
+                .map { value -> BitmapFactory.decodeResource(resources, value.imageRes) })
         startActivity(
             Intent(this@CameraActivity, AddIngredientActivity::class.java)
-                .apply { putParcelableArrayListExtra("Ingredients", foodList) })
+                .apply {
+                    putStringArrayListExtra("ingredients", filePath)
+                    putStringArrayListExtra(
+                        "nameList",
+                        FoodTypeFeatures.values().map { it.name } as ArrayList<String>)
+                })
+
     }
 
     private fun bitmapToByteArray(bitmaps: List<Bitmap>): List<ByteArray> {
@@ -52,16 +61,26 @@ class CameraActivity : BaseActivity<ActivityCameraBinding>(R.layout.activity_cam
         }
     }
 
-    private fun saveBitmapToJpeg(bitmap: Bitmap) {   // 선택한 이미지 내부 저장소에 저장
-        val tempFile = File(cacheDir, "image1") // 파일 경로와 이름 넣기
+    private fun saveBitmapToJpeg(bitmaps: List<Bitmap>): ArrayList<String> {   // 선택한 이미지 내부 저장소에 저장
         try {
-            tempFile.createNewFile() // 자동으로 빈 파일을 생성하기
-            val out = FileOutputStream(tempFile) // 파일을 쓸 수 있는 스트림을 준비하기
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out) // compress 함수를 사용해 스트림에 비트맵을 저장하기
-            out.close() // 스트림 닫아주기
-            Toast.makeText(applicationContext, "파일 저장 성공", Toast.LENGTH_SHORT).show()
+            val fileList = bitmaps.mapIndexed { index, bitmap ->
+                Log.e("Save Bitmap Image", cacheDir.absolutePath)
+                val tempFile = File(cacheDir, "imageTemp$index") // 파일 경로와 이름 넣기
+                tempFile.createNewFile() // 자동으로 빈 파일을 생성하기
+                val out = FileOutputStream(tempFile) // 파일을 쓸 수 있는 스트림을 준비하기
+                bitmap.compress(
+                    Bitmap.CompressFormat.JPEG,
+                    80,
+                    out
+                ) // compress 함수를 사용해 스트림에 비트맵을 저장하기
+                bitmap.recycle()
+                out.close() // 스트림 닫아주기
+                "imageTemp$index"
+            }
+            Log.e("GetIngredients", fileList.toString())
+            return fileList as ArrayList<String>
         } catch (e: Exception) {
-            Toast.makeText(applicationContext, "파일 저장 실패", Toast.LENGTH_SHORT).show()
+            return arrayListOf()
         }
     }
 
