614c23b1e81e585e53f4c5b5bd87f4059e55c39a
denkybrain@gmail.com <dnleo$312>
2022-11-10 03:21:41 +0900

    [FEAT] CameraX, ML Kit 라이브러리를 위한 파일 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("kotlin-kapt")
     id("kotlin-parcelize")
     id("com.google.gms.google-services")
+
 }
 
 val properties = Properties()
@@ -17,6 +18,8 @@ android {
     namespace = DefaultConfig.APPLICATION_ID
     compileSdk = DefaultConfig.COMPILE_SDK
 
+    val properties = Properties()
+    properties.load(project.rootProject.file("local.properties").inputStream())
     defaultConfig {
         applicationId = DefaultConfig.APPLICATION_ID
         minSdk = DefaultConfig.MIN_SDK
@@ -46,7 +49,6 @@ android {
         viewBinding = true
     }
 }
-
 dependencies {
     addFireBaseDependencies()
     addAndroidXDependencies()
@@ -66,4 +68,36 @@ dependencies {
     implementation(AndroidXDependencies.coroutines)
     implementation(ThirdPartyDependencies.gson)
     implementation(KotlinDependencies.kotlinxSerialization)
+
+
+
+    // Object detection feature with bundled default classifier
+    implementation("com.google.mlkit:object-detection:17.0.0")
+    // Object detection feature with custom classifier support
+    implementation("com.google.mlkit:object-detection-custom:17.0.0")
+    // On Device Machine Learnings
+    implementation("com.google.android.odml:image:1.0.0-beta1")
+
+    // CameraX
+    implementation ("androidx.camera:camera-camera2:1.0.0-SNAPSHOT")
+    implementation ("androidx.camera:camera-lifecycle:1.0.0-SNAPSHOT")
+    implementation ("androidx.camera:camera-view:1.0.0-SNAPSHOT")
+
+    // ViewModel and LiveData
+    implementation ("androidx.lifecycle:lifecycle-livedata:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:2.3.1")
+
+    // Selfie segmentation
+    implementation ("com.google.mlkit:segmentation-selfie:16.0.0-beta4")
+    implementation ("com.google.mlkit:camera:16.0.0-beta3")
+    // Image labeling custom
+    implementation("com.google.mlkit:image-labeling-custom:17.0.1")
+    //    // Or comment the dependency above and uncomment the dependency below to
+    //    // use unbundled model that depends on Google Play Services
+    //    // implementation 'com.google.android.gms:play-services-mlkit-image-labeling-custom:16.0.0-beta4'
+
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.google.guava:guava:27.1-android")
+    // Image labeling
+    implementation("com.google.mlkit:image-labeling:17.0.7")
 }
