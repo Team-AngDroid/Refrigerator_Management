67af46123c2049ac6e02709f80ecc7d4d79d1904
minho <my_alsgh@naver.com>
2022-11-11 04:36:38 +0900

    [Chore] Test Code Remove

--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/fragment/RefrigeratorFragment.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/fragment/RefrigeratorFragment.kt
@@ -1,12 +1,9 @@
 package com.angdroid.refrigerator_manament.presentation.home.fragment
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
-import com.angdroid.refrigerator_manament.BuildConfig
 import com.angdroid.refrigerator_manament.R
 import com.angdroid.refrigerator_manament.databinding.FragmentRefrigeratorBinding
 import com.angdroid.refrigerator_manament.presentation.custom.DynamicGridLayoutManager
@@ -15,13 +12,7 @@ import com.angdroid.refrigerator_manament.presentation.home.adapter.CategoryList
 import com.angdroid.refrigerator_manament.presentation.home.viewmodel.IngredientViewModel
 import com.angdroid.refrigerator_manament.presentation.util.BaseFragment
 import com.angdroid.refrigerator_manament.presentation.util.getSpanSizeLookUp
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.ktx.storage
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.ByteArrayOutputStream
-import java.util.*
 
 
 @AndroidEntryPoint
@@ -29,58 +20,12 @@ class RefrigeratorFragment :
     BaseFragment<FragmentRefrigeratorBinding>(R.layout.fragment_refrigerator) {
     private lateinit var adapter: CategoryListAdapter
     private val ingredientViewModel: IngredientViewModel by viewModels()
-    private val storageInstance: FirebaseStorage by lazy { Firebase.storage }
-
-    private val currentUserRef = storageInstance.reference
-    private var path: String = ""
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.ingredientViewModel = ingredientViewModel
         setAdapter()
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.kiwi)
-        val byteArray = bitmapToByteArray(bitmap)
-        val returnBitmap = byteArrayToBitmap(byteArray)
-
-
-        val outputStream = ByteArrayOutputStream()
-        returnBitmap?.compress(Bitmap.CompressFormat.JPEG, 90, outputStream)
-        val selectedImageBytes = outputStream.toByteArray()
-        //upLoadImage(selectedImageBytes)
-
-    }
-
-    private fun upLoadImage(byteArray: ByteArray) {
-        uploadMessageImage(byteArray) { imagePath ->
-            path = imagePath
-            Firebase.firestore.collection("Food").document("foodbase").set(mapOf("id" to imagePath))
-        }
-    }
-
-    fun pathToReference(path: String) = storageInstance.getReference(path)
-
-    private fun uploadMessageImage(
-        imageBytes: ByteArray,
-        onSuccess: (imagePath: String) -> Unit
-    ) {
-        val ref =
-            currentUserRef.child("${BuildConfig.USER_ID}/${UUID.nameUUIDFromBytes(imageBytes)}") //store image files in the firestorage
-        ref.putBytes(imageBytes).addOnSuccessListener {
-
-            onSuccess(ref.path)
-        }
-    }
-
-    private fun bitmapToByteArray(bitmap: Bitmap): ByteArray {
-        val stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-        return stream.toByteArray()
-    }
-
-    // Byte를 Bitmap으로 변환
-    private fun byteArrayToBitmap(byteArray: ByteArray): Bitmap? {
-        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
     }
 
     private fun setAdapter() {
--- app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/viewmodel/IngredientViewModel.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/presentation/home/viewmodel/IngredientViewModel.kt
@@ -1,12 +1,8 @@
 package com.angdroid.refrigerator_manament.presentation.home.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.angdroid.refrigerator_manament.domain.usecase.EnterRefrigeratorPageUseCase
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.ktx.storage
 import com.startup.meetiing.presentation.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -22,9 +18,6 @@ class IngredientViewModel @Inject constructor(
 ) : ViewModel() {
     private val _ingredient = MutableStateFlow<UiState>(UiState.Init)
     val ingredient get() = _ingredient.asStateFlow()
-
-    private val storageInstance: FirebaseStorage by lazy { Firebase.storage }
-    val uri = storageInstance.getReference("/x2pt8UANvml9SldmWItQ/2c5529bb-b61c-3116-8f01-056eeb6f03d1.jpeg").downloadUrl
     init {
         viewModelScope.launch {
             enterRefrigeratorPageUseCase().onStart {
