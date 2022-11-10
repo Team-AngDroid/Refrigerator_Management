fbe1acbb67b99310b9100320a3face1989fd5f0a
minho <my_alsgh@naver.com>
2022-11-11 04:38:55 +0900

    [Feat] Add Ingredients UseCase set Storage, FireStore

--- app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/AddUserFoodInfoUseCase.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/AddUserFoodInfoUseCase.kt
@@ -1,9 +1,11 @@
 package com.angdroid.refrigerator_manament.domain.usecase
 
+import com.angdroid.refrigerator_manament.domain.entity.model.IngredientType
+
 /**
  * 유저가 사진을 찍은 food에 대한 count와 날짜를 서버에 set
  * 식재료 추가
  */
 interface AddUserFoodInfoUseCase {
-    operator fun invoke(): Unit
+    suspend operator fun invoke(ingredients: List<IngredientType.Food>)
 }
--- app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/AddUserFoodInfoUseCaseImpl.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/domain/usecase/AddUserFoodInfoUseCaseImpl.kt
@@ -1,11 +1,44 @@
 package com.angdroid.refrigerator_manament.domain.usecase
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.angdroid.refrigerator_manament.domain.entity.model.IngredientType
 import com.angdroid.refrigerator_manament.domain.repository.FireBaseRepository
+import java.io.ByteArrayOutputStream
+import java.util.*
 import javax.inject.Inject
 
 class AddUserFoodInfoUseCaseImpl @Inject constructor(private val fireBaseRepository: FireBaseRepository) :
     AddUserFoodInfoUseCase {
-    override fun invoke() {
-        TODO("Not yet implemented")
+    override suspend fun invoke(
+        ingredients: List<IngredientType.Food>
+    ) {
+        val images = ingredients.map { food ->
+            if (food.image != "" && food.image != null) {
+                val bitmap =
+                    BitmapFactory.decodeFile("/data/user/0/com.angdroid.refrigerator_manament/cache/${food.image}")
+                val stream = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream)
+                bitmap.recycle()
+                stream.toByteArray()
+            } else {
+                null
+            }
+        }
+
+        val imagePath =
+            images.map { if (it != null) UUID.nameUUIDFromBytes(it).toString() else "" }
+        fireBaseRepository.upLoadFoodImage(imagePath, images)
+        fireBaseRepository.addIngredients(ingredients.mapIndexed { index, food ->
+            IngredientType.Food(
+                UUID.randomUUID().toString(),
+                food.foodId,
+                food.expirationDate,
+                food.name,
+                imagePath[index],
+                food.categoryId,
+                food.foodCount
+            )
+        })
     }
 }
