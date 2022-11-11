9f4caa9881de5aa3f94a41717f64af008640a684
IslandofDream <rhffldkt0517@naver.com>
2022-11-11 14:56:33 +0900

    [Feat] Add FCM Service class / manifest

---
+++ app/src/main/java/com/angdroid/refrigerator_manament/application/FirebaseMessagingService.kt
@@ -0,0 +1,14 @@
+package com.angdroid.refrigerator_manament.application
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+}
