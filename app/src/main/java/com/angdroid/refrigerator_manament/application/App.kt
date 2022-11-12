91e00feca456399260ca83c0b398041022d95bde
minho <my_alsgh@naver.com>
2022-11-12 18:46:54 +0900

    [Add] Remote, Cache Image PlaceHolder Add

--- app/src/main/java/com/angdroid/refrigerator_manament/application/App.kt
+++ app/src/main/java/com/angdroid/refrigerator_manament/application/App.kt
@@ -1,12 +1,8 @@
 package com.angdroid.refrigerator_manament.application
 
 import android.app.Application
-import android.content.Context
 import com.angdroid.refrigerator_manament.BuildConfig
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.angdroid.refrigerator_manament.domain.entity.model.IngredientType
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -24,7 +20,12 @@ class App : Application() {
 
     companion object {
         lateinit var userIngredientInfo: List<String> // Caching 느낌으로 남겨두자,, 얘만은 어떻게 하기가 애매함, BindingAdapter에도 붙어 있어서,,
+        lateinit var userIngredient: List<IngredientType.Food> // Caching 느낌으로 남겨두자,, 얘만은 어떻게 하기가 애매함, BindingAdapter에도 붙어 있어서,,
         private lateinit var instance: App
         fun getUserIngredientInfoInitialized(): Boolean = ::userIngredientInfo.isInitialized
+        fun setUserInfo(ingredients:List<IngredientType.Food>){
+            userIngredient = ingredients
+            userIngredientInfo = ingredients.map { it.name }
+        }
     }
 }
